b24eeeaf134325b51be379d3805cbfbc7d2a4e0b
GS Web Technologies <75965567+gswebtechnologies07@users.noreply.github.com>
2024-06-05 18:02:39 +0530

    install postgis

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,7 +8,7 @@ module.exports = [
       useDefaults: true,
       directives: {
         'connect-src': ["'self'", 'https:'],
-        'script-src': ["'self'", 'unsafe-inline', 'https://maps.googleapis.com','market-assets.strapi.io',],
+        'script-src': ["'self'", 'unsafe-inline', 'https://maps.googleapis.com','market-assets.strapi.io','*.tile.openstreetmap.org',],
         'media-src': [
           "'self'",
           'blob:',
